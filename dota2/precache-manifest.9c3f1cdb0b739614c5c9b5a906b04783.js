7d1554ff58dcedf6e79263faca927e392ef43416
seuminh <seuminh@yahoo.com>
2019-06-06 16:17:35 +0700

    Upload Dota2 website

---
+++ dota2/precache-manifest.9c3f1cdb0b739614c5c9b5a906b04783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e0cc574347936f44a299b30924ba38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2ad30005c052b8371c2d",
+    "url": "/static/css/main.c88de168.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "2ad30005c052b8371c2d",
+    "url": "/static/js/main.48faf2c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
